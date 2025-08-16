b8a48a1606db958f5260bee7c44395d1786ac988
Aishu15602 <aiswarya06211@gmail.com>
2025-08-16 21:06:21 +0530

    Add files via upload

---
+++ quote-project.java
@@ -0,0 +1,9 @@
+git config --global user.name "YourName"
+git config --global user.email "youremail@example.com"
+
+git clone https://github.com/username/simple-todo-project.git
+cd simple-todo-project
+copy path\to\todo.py .   # (Windows la)
+git add .
+git commit -m "Added To-Do List Project"
+git push origin main
