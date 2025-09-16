cf664ab49036eaf9dd996fb0310e868daa0cedd4
Aishu15602 <aiswarya06211@gmail.com>
2025-09-16 11:27:42 +0530

    Delete quote-project.java

--- quote-project.java
+++
@@ -1,9 +0,0 @@
-git config --global user.name "YourName"
-git config --global user.email "youremail@example.com"
-
-git clone https://github.com/username/simple-todo-project.git
-cd simple-todo-project
-copy path\to\todo.py .   # (Windows la)
-git add .
-git commit -m "Added To-Do List Project"
-git push origin main
